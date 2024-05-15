529165f2c069b583b65412afda3103b6935cd726
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2024-05-15 15:23:12 +0100

    Update trigger-github-action.js

--- api/trigger-github-action.js
+++ api/trigger-github-action.js
@@ -1,28 +1,44 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  const { scholarId } = req.body;
-  const token = process.env.GITHUB_TOKEN;
-  const repo = 'ezefranca/google-scholar';
-  const url = `https://api.github.com/repos/${repo}/dispatches`;
+  try {
+    const { scholarId } = req.body;
+    console.log('Received scholarId:', scholarId);
 
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      'Authorization': `token ${token}`
-    },
-    body: JSON.stringify({
-      event_type: 'fetch-scholar-publications',
-      client_payload: {
-        scholar_id: scholarId
-      }
-    })
-  });
+    const token = process.env.GITHUB_TOKEN; 
+    if (!token) {
+      console.error('GITHUB_TOKEN not set');
+      return res.status(500).json({ message: 'GITHUB_TOKEN not set' });
+    }
+    console.log('GITHUB_TOKEN is set');
 
-  if (response.ok) {
-    res.status(200).json({ message: 'GitHub Action triggered successfully.' });
-  } else {
-    res.status(500).json({ message: 'Error triggering GitHub Action.' });
+    const repo = 'ezefranca/google-scholar';
+    const url = `https://api.github.com/repos/${repo}/dispatches`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': `token ${token}`
+      },
+      body: JSON.stringify({
+        event_type: 'fetch-scholar-publications',
+        client_payload: {
+          scholar_id: scholarId
+        }
+      })
+    });
+
+    if (response.ok) {
+      console.log('GitHub Action triggered successfully');
+      res.status(200).json({ message: 'GitHub Action triggered successfully.' });
+    } else {
+      const errorText = await response.text();
+      console.error('Error triggering GitHub Action:', errorText);
+      res.status(500).json({ message: 'Error triggering GitHub Action.' });
+    }
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).json({ message: 'A server error has occurred' });
   }
 };
